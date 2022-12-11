7d3048c063daa7aaadb25068cd807957db480ef2
twosday <97022188+twosday@users.noreply.github.com>
2022-12-11 00:26:01 -0600

    Add files via upload

---
+++ _app/immutable/chunks/1-c0274797.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-7ecd4b78.js";export{t as component};
---
+++ _app/immutable/chunks/3-12392ed4.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/about/_page.svelte-b1c4b15b.js";export{t as component};
