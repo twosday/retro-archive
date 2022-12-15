4ba2718f5329b9ec712869b1b1f77b85ebf2cc45
twosday <97022188+twosday@users.noreply.github.com>
2022-12-15 03:35:12 -0600

    Add files via upload

---
+++ _app/immutable/chunks/2-6192aa4f.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-9ce1538c.js";export{t as component};
---
+++ _app/immutable/chunks/3-bbd9a9e8.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/about/_page.svelte-ad914ccf.js";export{t as component};
